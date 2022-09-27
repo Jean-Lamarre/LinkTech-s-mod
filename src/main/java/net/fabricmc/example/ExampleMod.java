1c99f1305383ff1a8a63d72762a9dbaf0a4836c5
Jean Lamarre <jonny.lamarre@hotmail.com>
2022-09-27 10:44:37 -0400

    First test tuto

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,11 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +15,19 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
 
+	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final Item CUSTOM_ITEM2 = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "meme_item"), CUSTOM_ITEM);
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "meme_item2"), CUSTOM_ITEM2);
 
 		LOGGER.info("Hello Fabric world!");
+
 	}
 }
